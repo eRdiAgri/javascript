261e5eda4f1e76ad2852ebb9ee0e14056e9b0b15
eяdi <101503629+eRdiAgri@users.noreply.github.com>
2022-08-10 09:53:45 +0300

    Add files via upload

---
+++ Javascript/1.js
@@ -0,0 +1,12 @@
+const message = "Hello world!";
+const bool = true;
+let num = 42;
+
+num = 5;
+// bool is false; // gives error
+
+const nonExistent = undefined;
+const empty = null;
+
+console.log(message, bool, num, nonExistent, empty);
+document.write(message);
---
+++ Javascript/2.js
@@ -0,0 +1,12 @@
+let a = 50;
+let b = 50;
+
+if (a<b) {
+    console.log("a<b");
+
+} else if (a>b) {
+    console.log("a>b");
+
+} else {
+    console.log("a == b");
+}
