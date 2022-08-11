1bb04d0b5454173d54288a834fca907775edd9f9
eяdi <101503629+eRdiAgri@users.noreply.github.com>
2022-08-11 13:30:40 +0300

    Add files via upload

---
+++ Javascript 2/javascript2 6.js
@@ -0,0 +1,28 @@
+const arr = [ 
+    {
+        val: 1,
+        name: "o1",
+    },
+
+    {
+        val: 4,
+        name: "o2",
+    },
+
+    {
+        val: 5,
+        name: "o3",
+    },
+
+    {
+        val: 8,
+        name: "o4",
+    }
+];
+
+console.log(arr);
+
+const biggerThan3 = arr.filter((el) => el.val > 3).map((el) => el.val);
+const biggerThanBool = arr.map((el) => el.val > 3).filter((el) => el);
+
+console.log(biggerThan3, biggerThanBool);
